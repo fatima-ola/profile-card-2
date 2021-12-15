258e9b6a054c0f0fd9b33b544e04f7e8ca163da8
fatima-ola <fatimaolasunkanmi01@gmail.com>
2021-12-15 20:22:59 +0100

    fixed error from avatar file

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Avatar = () => {
-  <img
-    src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/male-512.png"
-    alt="profile"
-    className="avatarPic"
-  />;
+  return (
+    <img
+      src="https://cdn0.iconfinder.com/data/icons/user-pictures/100/male-512.png"
+      alt="profile"
+      className="avatarPic"
+    />
+  );
 };
 
 export default Avatar;
